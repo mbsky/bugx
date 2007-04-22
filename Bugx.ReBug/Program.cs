d1403af1d87b169201ef3821c4fc4ac0809d468d
olivier.bossaer <olivier.bossaer@68e3e8bf-212d-0410-bc54-b330e369ac52>
2007-04-22 08:59:26 +0000

    Add LGPL licence

--- Bugx.ReBug/Program.cs
+++ Bugx.ReBug/Program.cs
@@ -1,3 +1,26 @@
+/*
+BUGx: An Asp.Net Bug Tracking tool.
+Copyright (C) 2007 Olivier Bossaer
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+Wavenet, hereby disclaims all copyright interest in
+the library `BUGx' (An Asp.Net Bug Tracking tool) written
+by Olivier Bossaer. (olivier.bossaer@gmail.com)
+*/
+
 using System;
 using System.Windows.Forms;
 using Microsoft.Win32;
