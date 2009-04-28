891dc97915e8a43ebb3b5a2987bc7926c2d76601
olivier.bossaer <olivier.bossaer@68e3e8bf-212d-0410-bc54-b330e369ac52>
2009-04-28 10:06:35 +0000

    Initial version

---
+++ Bugx.Test/EnvironmentWatermark.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace Bugx.Test
+{
+    /// <summary>
+    /// Summary description for EnvironmentWatermark
+    /// </summary>
+    public class EnvironmentWatermark:IHttpModule
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.PostAcquireRequestState += BeginRequest;
+        }
+
+        void BeginRequest(object sender, EventArgs e)
+        {
+            Page page = HttpContext.Current.Handler as Page;
+            if (page != null)
+            {
+                var script = page.ClientScript.GetWebResourceUrl(typeof(EnvironmentWatermark), "Bugx.Test.Script.js");
+                var images = page.ClientScript.GetWebResourceUrl(typeof(EnvironmentWatermark), "Bugx.Test.Images.png");
+                var css = page.ClientScript.GetWebResourceUrl(typeof(EnvironmentWatermark), "Bugx.Test.Stylesheet.css");
+                page.ClientScript.RegisterClientScriptInclude(typeof(EnvironmentWatermark), "ScriptInclude", script);
+                page.ClientScript.RegisterClientScriptBlock(typeof(EnvironmentWatermark), "Script", "<script type=\"text/javascript\" defer=\"defer\">/*<![CDATA[*/RegisterWatermark('" + images + "', '" + css + "');/*]]>*/</script>", false);
+            }
+        }
+    }
+}
---
+++ Bugx.Test/Script.js
@@ -0,0 +1,45 @@
+﻿function RegisterWatermark(imageUrl, css, text) {
+    if (/BugxTestHide=1/.test(document.cookie)) {
+        return;
+    }
+    function $createText(text, dest) {
+        var result = document.createTextNode(text || '');
+        if (dest) {
+            dest.appendChild(result);
+        }
+        return result;
+    }
+    function $create(e, attribs, styles) {
+        var result = document.createElement(e);
+        if (attribs) {
+            for (var attrib in attribs) {
+                result[attrib] = attribs[attrib];
+            }
+        }
+        if (styles) {
+            for (var style in styles) {
+                result.style[style] = styles[style];
+            }
+        }
+        result.injectInside = $injectInside;
+        return result;
+    }
+    function $injectInside(e) {
+        e.appendChild(this);
+        return this;
+    }
+    imageUrl = 'url(' + imageUrl + ')';
+
+    document.getElementsByTagName("head")[0].appendChild($create('link', { rel: 'stylesheet', type:'text/css', href:css }));
+    text = text || "Zone de préproduction: ";
+    document.title = text + document.title;
+    var host = $create("div", { className: 'Bugx_Test' }).injectInside(document.body);
+    var bar = $create("div").injectInside(host);
+    $create("b", null, {backgroundImage:imageUrl}).injectInside(bar);
+    $create("a", { href: "#" }, { backgroundImage: imageUrl }).injectInside(bar).onclick = function() {
+        host.style.display = 'none';
+        document.cookie = "BugxTestHide=1";
+        return false;
+    };
+    $createText(text, bar);
+}
