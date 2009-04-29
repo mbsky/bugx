299d5a219519214ae87bba2c645e32c52e79c69e
olivier.bossaer <olivier.bossaer@68e3e8bf-212d-0410-bc54-b330e369ac52>
2009-04-29 08:56:47 +0000

    Refactoring:  - Change the prototype name from Bugx.Test to Bugx.Watermark.  - Add full compliance to StyleCop / FxCop.  - Add Configuration properties:     <bugx>         <watermark>           <add key="Enable" value="true/false"/>           <add key="Text" value="Pre-production Environment"/>         </watermark>     </bugx>

--- Bugx.Test/Properties/AssemblyInfo.cs
+++ Bugx.Test/Properties/AssemblyInfo.cs
@@ -1,40 +1,30 @@
-﻿using System.Reflection;
+﻿// <copyright file="AssemblyInfo.cs" company="Wavenet">
+// Copyright © Wavenet 2009
+// </copyright>
+// <author>Olivier Bossaer</author>
+// <email>olivier.bossaer@gmail.com</email>
+// <date>2006-04-29</date>
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Web.UI;
+using System.Diagnostics.CodeAnalysis;
+using System;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Bugx.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Fortis Banque")]
-[assembly: AssemblyProduct("Bugx.Test")]
-[assembly: AssemblyCopyright("Copyright © Fortis Banque 2009")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyTitle("Bugx.Watermark")]
+[assembly: AssemblyDescription("Add a watermark on all pages.")]
+[assembly: AssemblyCompany("Wavenet")]
+[assembly: AssemblyProduct("Bugx.Watermark")]
+[assembly: AssemblyCopyright("Copyright © Wavenet 2009")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("868e7fd1-e5cb-4d52-8116-db20b43c5b51")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: WebResource("Bugx.Test.Script.js", "text/javascript")]
-[assembly: WebResource("Bugx.Test.Images.png", "image/png")]
-[assembly: WebResource("Bugx.Test.Stylesheet.css", "text/css")]
+[assembly: WebResource("Bugx.Watermark.Script.js", "text/javascript")]
+[assembly: WebResource("Bugx.Watermark.Images.png", "image/png")]
+[assembly: WebResource("Bugx.Watermark.Stylesheet.css", "text/css")]
+[assembly: CLSCompliant(true)]
+
+[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "namespace", Target = "Bugx.Watermark", MessageId = "Bugx")]
+[module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Bugx")]
